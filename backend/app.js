ce63cb770f889a024bdde37a21148352a9d3012c
Kay <joe_kayu@yahoo.com>
2020-10-04 19:54:38 +0100

    setup

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,62 @@
-import express from 'express';
-import logger from 'winston';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import passport from 'passport';
-import mongoose from 'mongoose';
-
-import { config } from './store/config';
-import { applyPassportStrategy } from './store/passport';
-import { userController, mailingController } from './controller';
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const debug = require('debug')('app:root')
+const Logger = require('./src/utils/logger')
+// import cors from 'cors';
+// import passport from 'passport';
 
 const app = express();
 
-// Set up CORS
-app.use(cors());
+// set up winston logger
+const log = new Logger('app_root');
 
-// Apply strategy to passport
-applyPassportStrategy(passport);
-app.use(bodyParser.urlencoded({ extended: true }));
+// require('dotenv').config()
+
+// mongoose.connect(process.env.DB_DEV_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+// const db = mongoose.connection;
+
+// app.set('trust proxy', 1);
+// const sessionOptions = {
+//   saveUninitialized: false,
+//   resave: false,
+//   secret: process.env.SECRET,
+//   cookie: {
+//     //secure: true,
+//     path: '/',
+//     httpOnly: true,
+//     maxAge: 600000 // 60 x 1000sec
+//   },
+//   store: new MongoStore({ mongooseConnection: mongoose.connection }),
+//   name: 'id'
+// }
+
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+//   log.info(`Connected to MongoDB: ${db.host}`);
+//   debug(`Connected to MongoDB: ${db.host}`);
+// });
+
+// Set up CORS
+// app.use(cors());
+
+// app.use(session(sessionOptions));
+app.use(morgan('tiny'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
 
 // API
-app.use('/', userController);
-app.use('/mailing', mailingController);
+// app.use('/', userController);
+// app.use('/mailing', mailingController);
 
-/**
- * Get port from environment and store in Express.
- */
-const { port, mongoDBUri, mongoHostName } = config.env;
-app.listen(port, () => {
-  logger.info(`Started successfully server at port ${port}`);
-  mongoose
-    .connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      logger.info(`Conneted to mongoDB at ${mongoHostName}`);
-    });
-});
+const port = process.env.PORT || 5000
+app.listen(port, function () {
+  log.info('Listening on port ...',port)
+  debug(`Listening on port ${port}...`)
+})
