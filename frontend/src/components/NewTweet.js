cdeb6d27af351089a9d4a375d798fd129f333f75
Kay <joe_kayu@yahoo.com>
2020-10-06 09:24:11 +0100

    toastify

--- frontend/src/components/NewTweet.js
+++ frontend/src/components/NewTweet.js
@@ -2,55 +2,47 @@ import React from "react";
 import styled from "styled-components";
 // import { toast } from "react-toastify";
 import TextareaAutosize from "react-textarea-autosize";
-// import { useQuery, useMutation } from "@apollo/react-hooks";
-// import { UploadFileIcon } from "../Icons";
-// import { displayError } from "../../utils";
 import Avatar from "./Avatar";
-// import { uploadImage } from "../../utils";
 import kay from '../kay.jpg';
 
 const NewTweet = () => {
   return (
     <Wrapper>
       <Avatar src={kay} alt="avatar" />
-      <form >
-        <div className="new-tweet">
-          <TextareaAutosize
-            max="160"
-            cols="48"
-            placeholder="What's happening?"
-            type="text"
-            className="text-area"
-            // value={tweet.value}
-            // onChange={tweet.onChange}
-          />
+      <form className="form">
+        <TextareaAutosize
+          max="160"
+          cols="55"
+          placeholder="What's happening?"
+          type="text"
+          className="text-area"
+          // value={tweet.value}
+          // onChange={tweet.onChange}
+        />
         <div className="globe">
           <i className="fa fa-globe mr-2" aria-hidden="true"></i> 
           <span>Everyone can reply</span>
         </div>
-          <div className="new-tweet-action">
-            <div className="blue">
-              <label htmlFor="file-input">
+        <div className="blue">
+          <label htmlFor="file-input">
                 <i className="fa fa-image mr-3" aria-hidden="true"></i> 
                 {/* <input id="file-input" accept="image/*" type="file"  /> */}
               </label>
-              <label htmlFor="gif-input">
+          <label htmlFor="gif-input">
                 <i className="fa fa-file mr-3" aria-hidden="true"></i> 
                 {/* <input id="gif-input" accept="image/*" type="file"  /> */}
               </label>
-              <label htmlFor="smiley">
+          <label htmlFor="smiley">
                 <i className="fa fa-smile mr-3" aria-hidden="true"></i> 
                 {/* <input id="smiley" accept="image/*" type="file"  /> */}
               </label>
-              <label htmlFor="schedule">
+          <label htmlFor="schedule">
                 <i className="fa fa-calendar-plus mr-3" aria-hidden="true"></i> 
                 {/* <input id="schedule" accept="image/*" type="file"  /> */}
               </label>
-                <button className="tweet-btn mr-0" disabled="true">
+          <button className="tweet-btn mr-0" disabled={true}>
                 Tweet
               </button>
-            </div>
-          </div>
         </div>
       </form>
     </Wrapper>
@@ -76,8 +68,9 @@ const Wrapper = styled.div`
 
   .tweet-btn{
     float:right;
+    border:none;
     background-color: #1da1f2;
-    padding:.3rem 1.5rem;
+    padding:.3rem 1.25rem;
     border-radius: 1.5rem;
     font-weight:700 ;
     font-size: 1rem;
