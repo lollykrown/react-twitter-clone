379fa948c17051a3226d39380179310895e5376e
Kay <joe_kayu@yahoo.com>
2020-10-07 15:31:19 +0100

    .

--- frontend/src/pages/Explore.js
+++ frontend/src/pages/Explore.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import styled from "styled-components";
 import Title from "../components/reusables/Title";
+import dp from '../dp.jpg';
 
 export default function Explore() {
     return (
-        <ExploreContainer className="home col-sm-10 col-md-10 col-lg-5">
+        <ExploreContainer className="home col-sm-10 col-md-10 col-lg-6">
             <Title title="explore" icon="null" />
+            <div className="banner-cont">
+            <img className="banner" src={dp} alt="banner"/>
+
+            </div>
             <div className="">
             <p>explore</p>
             </div>
@@ -15,5 +20,12 @@ export default function Explore() {
 
 const ExploreContainer = styled.div`
 
+.banner-cont{
+    height:28%
+}
+.banner{
+    width:100%;
+    height:100%
+}
 
 `;
